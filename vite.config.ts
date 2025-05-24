f5709ed6709418e31e19d2520ff23b3ddad419fc
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-05-24 18:43:33 +0900

    fix: postcss cjs 충돌 해결

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   css: {
-    postcss: './postcss/config.js',
+    postcss: './postcss/config.cjs',
   },
 });
