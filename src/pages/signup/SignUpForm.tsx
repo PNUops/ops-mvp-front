35e6e2c3048cb0dbc0f1c4618dce9c793558d22e
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-05-26 00:10:48 +0900

    refactor: SignUpForn 취소, 가입 버튼 form 내부로 이동

--- src/pages/signup/SignUpForm.tsx
+++ src/pages/signup/SignUpForm.tsx
@@ -3,55 +3,53 @@ import PasswordInput from '@components/PasswordInput';
 
 const SignUpForm = () => {
   return (
-    <div className="flex w-full flex-col items-end">
-      <form className="grid w-full grid-cols-[max-content_1fr_max-content] items-center gap-x-4 gap-y-6">
-        <label className="flex items-center gap-1">
-          <span className="text-red-500">*</span>
-          <span className="text-gray-500">이름</span>
-        </label>
-        <Input />
-        <div />
+    <form className="grid w-full grid-cols-[max-content_1fr_max-content] items-center gap-x-4 gap-y-6">
+      <label className="flex items-center gap-1">
+        <span className="text-red-500">*</span>
+        <span className="text-gray-500">이름</span>
+      </label>
+      <Input />
+      <div />
 
-        <label className="flex items-center gap-1">
-          <span className="text-red-500">*</span>
-          <span className="text-gray-500">학번</span>
-        </label>
-        <Input />
-        <div />
-        <Spacer />
+      <label className="flex items-center gap-1">
+        <span className="text-red-500">*</span>
+        <span className="text-gray-500">학번</span>
+      </label>
+      <Input />
+      <div />
+      <Spacer />
 
-        <label className="flex items-center gap-1">
-          <span className="text-red-500">*</span>
-          <span className="text-gray-500">이메일</span>
-        </label>
-        <Input placeholder="@pusan.ac.kr" />
-        <button className="rounded-lg border border-gray-300 p-3 px-4">인증코드 전송</button>
+      <label className="flex items-center gap-1">
+        <span className="text-red-500">*</span>
+        <span className="text-gray-500">이메일</span>
+      </label>
+      <Input placeholder="@pusan.ac.kr" />
+      <button className="rounded-lg border border-gray-300 p-3 px-4">인증코드 전송</button>
 
-        <label />
-        <Input placeholder="인증코드 입력" />
-        <button className="rounded-lg border border-gray-300 p-3 px-4">확인</button>
-        <Spacer />
+      <label />
+      <Input placeholder="인증코드 입력" />
+      <button className="rounded-lg border border-gray-300 p-3 px-4">확인</button>
+      <Spacer />
 
-        <label className="flex items-center gap-1">
-          <span className="text-red-500">*</span>
-          <span className="text-gray-500">비밀번호</span>
-        </label>
-        <PasswordInput value="" setValue={() => {}} />
-        <div />
+      <label className="flex items-center gap-1">
+        <span className="text-red-500">*</span>
+        <span className="text-gray-500">비밀번호</span>
+      </label>
+      <PasswordInput value="" setValue={() => {}} />
+      <div />
 
-        <label className="flex items-center gap-1">
-          <span className="text-red-500">*</span>
-          <span className="text-gray-500">비밀번호 확인</span>
-        </label>
-        <PasswordInput value="" setValue={() => {}} placeholder="비밀번호 확인" />
-        <div />
-      </form>
+      <label className="flex items-center gap-1">
+        <span className="text-red-500">*</span>
+        <span className="text-gray-500">비밀번호 확인</span>
+      </label>
+      <PasswordInput value="" setValue={() => {}} placeholder="비밀번호 확인" />
+      <div />
 
-      <div className="mt-12 flex gap-4">
-        <button className="min-w-32 rounded-full border border-gray-200 p-3">취소</button>
-        <button className="min-w-32 rounded-full bg-gray-200 p-3">가입</button>
+      <div className="col-span-3 mt-8 flex justify-end gap-4">
+        <button className="w-32 rounded-full border border-gray-200 p-3">취소</button>
+        <button className="w-32 rounded-full bg-gray-200 p-3">가입</button>
       </div>
-    </div>
+    </form>
   );
 };
 export default SignUpForm;
