71ddd7f3187a782454394368e742b05f4c596627
HanseKim <kimbird2002@naver.com>
2025-05-27 00:22:46 +0900

    style: 관리자페이지 코드리뷰 토대로 리팩토링

---
+++ src/pages/admin/ProjectSubmissionTable.tsx
@@ -0,0 +1,73 @@
+interface Submission {
+  id: number;
+  teamName: string;
+  projectName: string;
+  isSubmitted: boolean;
+  likes: number;
+}
+
+interface Props {
+  submissions: Submission[];
+  type: 'project' | 'vote';
+}
+
+const TableHead = ({ type }: { type: 'project' | 'vote' }) => {
+  return (
+    <thead className="bg-gray-100">
+      <tr>
+        <th className="w-[5%] border-r border-b border-gray-300 p-2 text-sm">{type === 'project' ? '순번' : '순위'}</th>
+        <th className="w-[20%] border-r border-b border-gray-300 p-2 text-sm">팀명</th>
+        <th className="w-[55%] border-r border-b border-gray-300 p-2 text-sm">작품명</th>
+        <th className="w-[20%] border-b border-gray-300 p-2 text-sm">
+          {type === 'project' ? '제출여부' : '좋아요 수'}
+        </th>
+      </tr>
+    </thead>
+  );
+};
+
+const TableBody = ({ submissions, type }: Props) => {
+  return (
+    <tbody>
+      {submissions.map((item: Submission, idx: number) => (
+        <tr key={item.id} className="">
+          <td className="w-[10%] border-r border-b border-gray-300 p-2 py-3 pl-4 text-sm">{idx + 1}</td>
+          <td className="w-[20%] border-r border-b border-gray-300 p-2 py-3 pl-4 text-sm">{item.teamName}</td>
+          <td className="w-[50%] border-r border-b border-gray-300 p-2 py-3 pl-4 text-sm">{item.projectName}</td>
+          {type === 'vote' ? (
+            <td className="w-[20%] border-b border-gray-300 p-2 py-3 pl-4 text-sm">
+              <div className="flex h-[30px] w-[100px] items-center">
+                <span>좋아요&nbsp;&nbsp;</span>
+                <span className="font-bold">{item.likes.toLocaleString()}</span>개
+              </div>
+            </td>
+          ) : (
+            <td className="w-[20%] border-b border-gray-300 p-2 py-3 pl-4 text-sm">
+              <div
+                className={`flex h-[35px] w-[80px] items-center justify-center rounded-md text-center ${
+                  item.isSubmitted ? 'bg-mainBlue' : 'bg-mainRed'
+                } px-2 py-[4px] text-sm text-white`}
+              >
+                {item.isSubmitted ? '제출완료' : '미제출'}
+              </div>
+            </td>
+          )}
+        </tr>
+      ))}
+    </tbody>
+  );
+};
+
+const ProjectSubmissionTable = ({ submissions, type }: Props) => {
+  return (
+    <section className="mb-8">
+      <h2 className="mb-8 text-2xl font-bold">{type === 'project' ? '프로젝트 등록현황' : '좋아요 랭킹'}</h2>
+      <table className="w-full border-collapse bg-white">
+        <TableHead type={type} />
+        <TableBody submissions={submissions} type={type} />
+      </table>
+    </section>
+  );
+};
+
+export default ProjectSubmissionTable;
--- src/pages/admin/VoteRate.tsx
+++ src/pages/admin/VoteRate.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { PieChart, Pie, Cell } from 'recharts';
 
 interface PieItem {
@@ -6,14 +5,14 @@ interface PieItem {
   value: number;
 }
 
-interface PieData {
+interface Props {
   pieData: PieItem[];
   totalVotes: number;
   participationRate: number;
   pieColors: string[];
 }
 
-const VoteRate: React.FC<PieData> = ({ pieData, totalVotes, participationRate, pieColors }) => {
+const VoteRate = ({ pieData, totalVotes, participationRate, pieColors }: Props) => {
   return (
     <section className="">
       <h2 className="mb-4 text-2xl font-bold">투표 참여율</h2>
@@ -23,7 +22,6 @@ const VoteRate: React.FC<PieData> = ({ pieData, totalVotes, participationRate, p
       >
         <div className="flex justify-center">
           <PieChart width={150} height={150}>
-            {/* 1. 회색 배경 원 전체 */}
             <Pie
               data={[{ name: '배경', value: 100 }]}
               cx="50%"
@@ -35,10 +33,8 @@ const VoteRate: React.FC<PieData> = ({ pieData, totalVotes, participationRate, p
               dataKey="value"
               cornerRadius={0}
             >
-              <Cell fill={pieColors[1]} stroke="none" /> {/* 회색 */}
+              <Cell fill={pieColors[1]} stroke="none" />
             </Pie>
-
-            {/* 2. 초록 참여율 조각만 위에 덧칠 */}
             <Pie
               data={[{ name: '참여', value: participationRate }]}
               cx="50%"
@@ -50,23 +46,8 @@ const VoteRate: React.FC<PieData> = ({ pieData, totalVotes, participationRate, p
               dataKey="value"
               cornerRadius={10}
             >
-              <Cell fill={pieColors[0]} /> {/* 초록 */}
+              <Cell fill={pieColors[0]} />
             </Pie>
-
-            {/* <Pie
-              data={pieData}
-              cx="50%"
-              cy="50%"
-              innerRadius={50}
-              outerRadius={70}
-              dataKey="value"
-              startAngle={-270}
-              endAngle={90}
-            >
-              {pieData.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={pieColors[index]} />
-              ))}
-            </Pie> */}
           </PieChart>
         </div>
         <p className="-mt-20 text-xl font-bold text-green-600">{participationRate}%</p>
