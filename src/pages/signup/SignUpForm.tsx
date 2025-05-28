62d7a8f738a8febc21c05799ae9665dc178e7ab3
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-05-28 15:01:02 +0900

    design: 커스텀 컬러 적용

--- src/pages/signup/SignUpForm.tsx
+++ src/pages/signup/SignUpForm.tsx
@@ -5,49 +5,49 @@ const SignUpForm = () => {
   return (
     <form className="grid w-full grid-cols-[max-content_1fr_max-content] items-center gap-x-4 gap-y-6">
       <label className="flex items-center gap-1">
-        <span className="text-red-500">*</span>
-        <span className="text-gray-500">이름</span>
+        <span className="text-mainRed">*</span>
+        <span className="text-midGray">이름</span>
       </label>
       <Input />
       <div />
 
       <label className="flex items-center gap-1">
-        <span className="text-red-500">*</span>
-        <span className="text-gray-500">학번</span>
+        <span className="text-mainRed">*</span>
+        <span className="text-midGray">학번</span>
       </label>
       <Input />
       <div />
       <Spacer />
 
       <label className="flex items-center gap-1">
-        <span className="text-red-500">*</span>
-        <span className="text-gray-500">이메일</span>
+        <span className="text-mainRed">*</span>
+        <span className="text-midGray">이메일</span>
       </label>
       <Input placeholder="@pusan.ac.kr" />
-      <button className="rounded-lg border border-gray-300 p-3 px-4">인증코드 전송</button>
+      <button className="border-lightGray rounded-lg border p-3 px-4">인증코드 전송</button>
 
       <label />
       <Input placeholder="인증코드 입력" />
-      <button className="rounded-lg border border-gray-300 p-3 px-4">확인</button>
+      <button className="border-lightGray rounded-lg border p-3 px-4">확인</button>
       <Spacer />
 
       <label className="flex items-center gap-1">
-        <span className="text-red-500">*</span>
-        <span className="text-gray-500">비밀번호</span>
+        <span className="text-mainRed">*</span>
+        <span className="text-midGray">비밀번호</span>
       </label>
       <PasswordInput value="" setValue={() => {}} />
       <div />
 
       <label className="flex items-center gap-1">
-        <span className="text-red-500">*</span>
-        <span className="text-gray-500">비밀번호 확인</span>
+        <span className="text-mainRed">*</span>
+        <span className="text-midGray">비밀번호 확인</span>
       </label>
       <PasswordInput value="" setValue={() => {}} placeholder="비밀번호 확인" />
       <div />
 
       <div className="col-span-3 mt-8 flex justify-end gap-4">
-        <button className="w-32 rounded-full border border-gray-200 p-3">취소</button>
-        <button className="w-32 rounded-full bg-gray-200 p-3">가입</button>
+        <button className="border-lightGray w-32 rounded-full border p-3">취소</button>
+        <button className="bg-lightGray w-32 rounded-full p-3">가입</button>
       </div>
     </form>
   );
