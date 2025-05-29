00fc964cae67665c9e9c0cb8e08ae80158a7b9cf
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-05-29 20:12:51 +0900

    refactor: UserStore setUser 타입 수정

--- src/stores/useUserStore.ts
+++ src/stores/useUserStore.ts
@@ -3,10 +3,10 @@ import { create } from 'zustand';
 
 type UserState = {
   user: User | null;
-  setUser: (user: User) => void;
+  setUser: (user: User | null) => void;
 };
 
 export const useUserStore = create<UserState>((set) => ({
   user: null,
-  setUser: (user: User) => set({ user }),
+  setUser: (user: User | null) => set({ user }),
 }));
