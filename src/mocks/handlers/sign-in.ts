e280a9de1780c6ec9991cf9c1eefe3f2775fba92
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-05-29 22:58:10 +0900

    refactor: signInHandler 내부 타입 수정

--- src/mocks/handlers/sign-in.ts
+++ src/mocks/handlers/sign-in.ts
@@ -1,9 +1,10 @@
 import { mockSignInResponse } from '@mocks/data/sign-in';
 import { http, HttpResponse } from 'msw';
+import { SignInRequestDto } from 'types/DTO';
 
 export const signInHandlers = [
   http.post('/api/sign-in', async ({ request }) => {
-    const { email, password } = (await request.json()) as { email: string; password: string };
+    const { email, password } = (await request.json()) as SignInRequestDto;
 
     if (email === 'test@test.com' && password === 'test') {
       return HttpResponse.json(mockSignInResponse);
