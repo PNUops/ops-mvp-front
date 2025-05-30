3c8c665e0c42575ee67024f55bc75fc9a84a864f
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-05-30 19:41:44 +0900

    feat: 이메일 인증 코드 api 요청 함수 정의

--- src/apis/signUp.ts
+++ src/apis/signUp.ts
@@ -1,7 +1,11 @@
-import { EmailVerificationRequestDTO } from 'types/DTO';
+import { EmailVerificationCodeRequestDTO, EmailVerificationRequestDTO } from 'types/DTO';
 import apiClient from './apiClient';
 
 export const postEmailVerification = async (request: EmailVerificationRequestDTO) => {
   const response = await apiClient.post('/sign-up/email-auth', request);
+  return response.data;
+};
+export const patchEmailVerificationCode = async (request: EmailVerificationCodeRequestDTO) => {
+  const response = await apiClient.patch('/sign-up/email-auth', request);
   return response.data;
 };
