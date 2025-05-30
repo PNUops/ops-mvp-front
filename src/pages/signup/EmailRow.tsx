8f0e93e9f38426fb7884046733af4f1b88e63b24
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-05-30 18:55:19 +0900

    feat: 인증메일 전송 로직 구현

--- src/pages/signup/EmailRow.tsx
+++ src/pages/signup/EmailRow.tsx
@@ -1,19 +1,42 @@
 import Input from '@components/Input';
+import { useMutation } from '@tanstack/react-query';
+import { postEmailVerification } from 'apis/signUp';
+import { isPNUEmail } from 'utils/email';
 
 interface Props {
   value: string;
   setValue: (value: string) => void;
 }
 
-const EmailRow = ({ value, setValue }: Props) => {
+const EmailRow = ({ value: email, setValue: setEmail }: Props) => {
+  const mutation = useMutation({
+    mutationFn: postEmailVerification,
+    onSuccess: () => {
+      alert(`${email}로 인증 메일을 전송했어요`);
+    },
+    onError: (error) => {
+      alert(`인증 메일 전송에 실패했어요: ${error.message}`);
+    },
+  });
+
+  const handleSendCode = () => {
+    if (!isPNUEmail(email)) {
+      alert('부산대학교 이메일(@pusan.ac.kr)만 사용할 수 있습니다.');
+    } else {
+      mutation.mutate({ email });
+    }
+  };
+
   return (
     <>
       <label className="flex items-center gap-1">
         <span className="text-mainRed">*</span>
         <span className="text-midGray">이메일</span>
       </label>
-      <Input placeholder="@pusan.ac.kr" value={value} onChange={(e) => setValue(e.target.value)} />
-      <button className="border-lightGray hover:bg-lightGray rounded-lg border p-3 px-4">인증코드 전송</button>
+      <Input placeholder="@pusan.ac.kr" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <button onClick={handleSendCode} className="border-lightGray hover:bg-lightGray rounded-lg border p-3 px-4">
+        인증코드 전송
+      </button>
     </>
   );
 };
