98b6ab6821c19f1a7f45d23653a05056cdac8a22
redzzzi <myqueen1346@naver.com>
2025-06-01 05:39:45 +0900

    feature: ProjectViewerPage UI - Carousel, Project contents, Comment section

---
+++ src/pages/project-viewer/Carousel.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+
+import { team_images as images } from '@mocks/data/viewer';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa6';
+
+const Carousel = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goToPrev = () => {
+    setCurrentIndex((prev) => (prev === 0 ? images.length - 1 : prev - 1));
+  };
+
+  const goToNext = () => {
+    setCurrentIndex((prev) => (prev === images.length - 1 ? 0 : prev + 1));
+  };
+
+  const goToSlide = (index: number) => {
+    setCurrentIndex(index);
+  };
+
+  return (
+    <div className="flex items-center justify-center gap-10 px-15">
+      <button onClick={goToPrev}>
+        <FaChevronLeft size={35} className="text-midGray hover:text-mainGreen" />
+      </button>
+
+      <div className="flex w-full flex-col">
+        <div className="border-midGray aspect-[3/2] h-[300px] w-full rounded border">
+          <img src={images[currentIndex].imageId} alt="Project image" className="h-full w-full object-cover" />
+        </div>
+        <div className="mt-4 flex justify-center gap-3 px-3">
+          {images.map((_, index) => (
+            <button
+              key={index}
+              onClick={() => goToSlide(index)}
+              className={`h-1 w-full rounded-full ${currentIndex === index ? 'bg-mainGreen' : 'bg-lightGray'}`}
+            />
+          ))}
+        </div>
+      </div>
+      <button onClick={goToNext}>
+        <FaChevronRight size={35} className="text-midGray hover:text-mainGreen" />
+      </button>
+    </div>
+  );
+};
+
+export default Carousel;
--- src/pages/project-viewer/ProjectViewerPage.tsx
+++ src/pages/project-viewer/ProjectViewerPage.tsx
@@ -1,5 +1,66 @@
-const ProjectViewer = () => {
-  return <div>project viewer 페이지입니다.</div>;
+import React, { useState, useRef } from 'react';
+import Carousel from './Carousel';
+import CommentSection from './CommentSection';
+import { GoPencil } from 'react-icons/go';
+import { FaHeart } from 'react-icons/fa';
+import { PiCrown } from 'react-icons/pi';
+import { IoPersonOutline } from 'react-icons/io5';
+
+import { project_view } from '@mocks/data/viewer';
+import MediaEmbedder from './MediaEmbedder';
+
+const ProjectViewerPage = () => {
+  const { projectName, teamName, isLiked, overview, leaderName, participants, githubPath, youTubePath } = project_view;
+
+  return (
+    <div>
+      <div className="flex gap-19">
+        <div className="text-[36px] font-bold">{projectName}</div>
+        <button className="border-midGray flex items-center gap-3 rounded-full border px-10 py-4 text-sm">
+          <GoPencil />
+          수정하기
+        </button>
+      </div>
+      <div className="text-smbold flex font-bold">{teamName}</div>
+      <div className="h-10" />
+      <Carousel />
+      <div className="h-10" />
+      <button
+        className={`bg-${isLiked ? 'mainGreen' : 'lightGray'} relative flex items-center gap-3 justify-self-center rounded-full px-30 py-4 text-sm text-white`}
+      >
+        <FaHeart className={`absolute left-7 text-${isLiked ? 'white' : 'midGray'}`} size={25} />
+        좋아요
+      </button>
+      <div className="h-20" />
+      <div className="flex flex-col gap-5">
+        <div className="text-title font-bold">Overview</div>
+        <div className="text-sm leading-[1.8]">{overview}</div>
+      </div>
+      <div className="h-20" />
+      <div className="flex flex-col gap-5">
+        <div className="text-title font-bold">Participants</div>
+        <span className="flex items-center gap-3">
+          <PiCrown />
+          <span className="text-sm">{leaderName}</span>
+        </span>
+        <span className="flex items-center gap-3">
+          <IoPersonOutline />
+          {participants.map((name, index) => (
+            <span key={index} className="text-sm">
+              {name}
+            </span>
+          ))}
+        </span>
+      </div>
+      <div className="h-20" />
+      <div className="flex flex-col gap-5">
+        <div className="text-title font-bold">URL</div>
+        <MediaEmbedder githubUrl={githubPath} youtubeUrl={youTubePath} />
+      </div>
+      <div className="h-20" />
+      <CommentSection />
+    </div>
+  );
 };
 
-export default ProjectViewer;
+export default ProjectViewerPage;
