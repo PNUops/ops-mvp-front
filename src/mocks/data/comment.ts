24231be6da8097cd991fa8c822923191dc27e5cd
redzzzi <myqueen1346@naver.com>
2025-06-01 05:42:38 +0900

    chore: test data for ProjectViewerPage

---
+++ src/mocks/data/comment.ts
@@ -0,0 +1,39 @@
+export const comment_list = [
+  {
+    commentId: 1,
+    description: '정말 멋진 프로젝트네영!!',
+    memberId: 101,
+    teamId: 5,
+  },
+  {
+    commentId: 2,
+    description: '화이팅입니다 ㅎㅎ!',
+    memberId: 102,
+    teamId: 5,
+  },
+  {
+    commentId: 3,
+    description: '대단한 작업이네요! 계속 힘내세요!',
+    memberId: 103,
+    teamId: 5,
+  },
+  {
+    commentId: 4,
+    description:
+      '진짜 멋진 아이디어! 꼭 성공하시길 바랍니다. 진짜 멋진 아이디어! 꼭 성공하시길 바랍니다. 진짜 멋진 아이디어! 꼭 성공하시길 바랍니다. 진짜 멋진 아이디어! 꼭 성공하시길 바랍니다. 진짜 멋진 아이디어! 꼭 성공하시길 바랍니다. 진짜 멋진 아이디어! 꼭 성공하시길 바랍니다. 진짜 멋진 아이디어! 꼭 성공하시길 바랍니다. ',
+    memberId: 104,
+    teamId: 5,
+  },
+  {
+    commentId: 5,
+    description: '너무 좋은 프로젝트입니다! 기대하고 있어요!',
+    memberId: 105,
+    teamId: 5,
+  },
+  {
+    commentId: 6,
+    description: '아이디어가 정말 창의적이에요. 좋은 결과 있기를 바랍니다.',
+    memberId: 106,
+    teamId: 5,
+  },
+];
