f878b1071526f4bf1250eea6892af09b873a29aa
latteeea <kimtaeran4767@gmail.com>
2025-06-02 00:36:41 +0900

    design: 커스텀 색상 적용

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -4,15 +4,6 @@ import TotalCard from "@pages/main/TotalCard";
 const MainPage = () => {
   return (
       <div className="flex flex-col gap-20">
-          <div className="h-spacing-header bg-white shadow">
-              {/*헤더자리*/}
-          </div>
-
-          <div className="flex flex-1">
-              <aside className="w-spacing-sidebar bg-gray-100 border-r" >
-                  {/*사이드바*/}
-              </aside>
-          </div>
         <Notice/>
         <TotalCard />
       </div>
