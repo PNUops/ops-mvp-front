fd4dce6f195c9ab11bd3444913146c4a6d5aaa9f
redzzzi <myqueen1346@naver.com>
2025-06-01 21:17:50 +0900

    refactor: integrate MainLayout with FullContainer only - MainLayout 구성 요소였던 Sidebar를 제외한다. - Sidebar가 제외된 후 유사한 기능을 하는 FitContainer은 모두 FullContainer로 대체한다. - MainLayout을 구성하는 고정된 요소는 Header, FullContainer, Footer, Outlet으로 한다. - Outlet 페이지 컴포넌트들의 경로를 수정한다.

--- src/layout/FullContainer.tsx
+++ src/layout/FullContainer.tsx
@@ -5,8 +5,7 @@ import Header from './Header';
 const FullContainer = () => {
   return (
     <div className="flex min-h-screen w-full flex-col items-center">
-      <Header />
-      <div className="w-full max-w-[1448px] flex-1 px-2 pb-20 2xl:px-0">
+      <div className="w-full max-w-5xl flex-1 px-10 pt-5 pb-20 2xl:px-0">
         <Outlet />
       </div>
     </div>
--- src/route/AppRoutes.tsx
+++ src/route/AppRoutes.tsx
@@ -1,56 +1,26 @@
 import { useRoutes } from 'react-router-dom';
 
 import MainLayout from '@layout/MainLayout';
-import FullContainer from '@layout/FullContainer';
-import FitContainer from '@layout/FitContainer';
+
 import MainPage from '@pages/main/MainPage';
 import ProjectEditorPage from '@pages/project-editor/ProjectEditorPage';
-// import ProjectViewerPage from '@pages/project-viewer/ProjectViewerPage';
+import ProjectViewerPage from '@pages/project-viewer/ProjectViewerPage';
 import SignInPage from '@pages/signin/SignInPage';
 import SignUpPage from '@pages/signup/SignUpPage';
 import AdminPage from '@pages/admin/AdminPage';
-import ProjectViewerPage from '@pages/project-viewer/ProjectViewerPage';
 
 const AppRoutes = () =>
   useRoutes([
     {
-      element: <FullContainer />,
-      children: [
-        {
-          path: '/signin',
-          element: <SignInPage />,
-        },
-        {
-          path: '/signup',
-          element: <SignUpPage />,
-        },
-      ],
-    },
-    {
       path: '/',
       element: <MainLayout />,
       children: [
-        {
-          element: <FitContainer />,
-          children: [
-            {
-              index: true,
-              element: <MainPage />,
-            },
-            {
-              path: 'view',
-              element: <ProjectViewerPage />,
-            },
-            {
-              path: 'edit',
-              element: <ProjectEditorPage />,
-            },
-            {
-              path: 'monitor',
-              element: <AdminPage />,
-            },
-          ],
-        },
+        { index: true, element: <MainPage /> },
+        { path: 'signin', element: <SignInPage /> },
+        { path: 'signup', element: <SignUpPage /> },
+        { path: 'view', element: <ProjectViewerPage /> },
+        { path: 'edit', element: <ProjectEditorPage /> },
+        { path: 'monitor', element: <AdminPage /> },
       ],
     },
   ]);
