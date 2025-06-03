039b61a10eec48c90487064b28ddad02c84e751e
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-06-03 21:47:39 +0900

    design: IdTab 임시 구현

---
+++ src/pages/find/IdTab.tsx
@@ -0,0 +1,26 @@
+import Input from '@components/Input';
+import RoundedButton from '@components/RoundedButton';
+import { useState } from 'react';
+
+const IdTab = () => {
+  const [value, setValue] = useState('');
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="flex max-w-md gap-4">
+        <label className="flex min-w-fit items-center gap-1">
+          <span className="text-mainRed">*</span>
+          <span className="text-midGray">학번</span>
+        </label>
+        <Input
+          value={value}
+          onChange={(e) => {
+            const input = e.target.value;
+            if (/^\d*$/.test(input)) setValue(input);
+          }}
+        />
+      </div>
+      <RoundedButton className="mx-auto">아이디 찾기</RoundedButton>
+    </div>
+  );
+};
+export default IdTab;
