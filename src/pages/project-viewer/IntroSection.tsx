2f20c3d2e06b382f6810f36de72e58f6df41dfbf
redzzzi <myqueen1346@naver.com>
2025-06-04 04:08:19 +0900

    feature: intro of viewer page - 역할 타입의 종류와 memberId-leaderId 간 비교로 수정 버튼의 접근을 제한하고자 했습니다.

--- src/pages/project-viewer/IntroSection.tsx
+++ src/pages/project-viewer/IntroSection.tsx
@@ -1,20 +1,26 @@
 import useAuth from 'hooks/useAuth';
+import { useUserStore } from 'stores/useUserStore';
 import { GoPencil } from 'react-icons/go';
+import { useNavigate } from 'react-router-dom';
 
 interface IntroSectionProps {
+  teamId: number;
+  leaderId: number;
   projectName: string;
   teamName: string;
 }
 
-const IntroSection = ({ projectName, teamName }: IntroSectionProps) => {
+const IntroSection = ({ teamId, leaderId, projectName, teamName }: IntroSectionProps) => {
   const { isLeader } = useAuth();
+  const memberId = useUserStore((state) => state.user?.id);
+  const navigate = useNavigate();
 
   return (
     <>
       <div className="flex items-center gap-10">
         <div className="text-[36px] font-bold">{projectName}</div>
-        {isLeader && (
-          <button className="border-midGray text-exsm flex items-center gap-3 rounded-full border px-5 py-1 text-gray-700 hover:cursor-pointer">
+        {isLeader && (memberId === leaderId) &&  (
+          <button onClick={() => navigate(`/teams/edit/${teamId}`)}className="border-midGray text-exsm flex items-center gap-3 rounded-full border px-5 py-1 text-gray-700 hover:cursor-pointer">
             <GoPencil />
             수정하기
           </button>
