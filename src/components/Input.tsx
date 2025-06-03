219eb91a81d84cd23053db58b1189165cefa3822
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-06-03 21:32:08 +0900

    design: Input 컴포넌트 border 색상 수정

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,7 +5,7 @@ interface Props extends InputHTMLAttributes<HTMLInputElement> {}
 const Input = ({ className = '', ...props }: Props) => {
   return (
     <input
-      className={`border-midGray focus:outline-mainGreen w-full rounded-lg border p-3 text-lg focus:outline-2 ${className}`}
+      className={`border-lightGray focus:outline-mainGreen w-full rounded-lg border p-3 text-lg focus:outline-2 ${className}`}
       {...props}
     />
   );
