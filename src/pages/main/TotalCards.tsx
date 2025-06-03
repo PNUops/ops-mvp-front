54ca87a951caf411cc63793f8641ddf122a8a907
latteeea <kimtaeran4767@gmail.com>
2025-06-03 22:00:09 +0900

    feat: 팀장일 때 프로젝트 에디터 버튼 추가

--- src/pages/main/TotalCards.tsx
+++ src/pages/main/TotalCards.tsx
@@ -5,12 +5,13 @@ import {useQuery} from "@tanstack/react-query"
 import {getAllTeams, getSubmissionStatus} from "../../apis/teams";
 import {TeamListItemDto} from "../../types/DTO/teamListDto";
 import {SubmissionStatusDto} from "../../types/DTO/submissionStatusDto";
+import {Link} from "react-router-dom";
+import { TbPencil } from "react-icons/tb";
 
 
 const TotalCards = () => {
 
-    const {isLeader, user} = useAuth();
-
+    const { isLeader, user } = useAuth();
     const {
         data: submissionData,
     } = useQuery<SubmissionStatusDto>({
@@ -26,35 +27,44 @@ const TotalCards = () => {
         queryFn: getAllTeams,
     });
 
-
-    console.log("isLeader", isLeader);
     console.log("teams", teams);
     console.log("submissionData", submissionData);
 
     const showLeaderMessage = isLeader && submissionData?.isSubmitted === false;
 
     return (
-        <div id="projects" className="flex flex-col gap-4">
+      <div id="projects" className="flex flex-col gap-4">
+        {isLeader && submissionData?.teamId && (
+          <div className="flex justify-between items-center px-4">
             <h3 id="projects" className="text-sm font-bold">현재 투표진행중인 작품</h3>
+            <Link
+              to={`/teams/edit/${submissionData.teamId}`}
+              className="flex items-center gap-2 bg-mainGreen text-white font-inter font-bold text-[18px] leading-[100%] rounded-full px-5 py-3"
+            >
+              <TbPencil size={16} strokeWidth={2} />
+              프로젝트 에디터
+            </Link>
+          </div>
+        )}
 
+        {showLeaderMessage && <LeaderMessage leaderName={user?.name ?? '팀장'} />}
 
-            {showLeaderMessage && <LeaderMessage leaderName={user?.name ?? "팀장" } />}
-
-            <section aria-labelledby="allCards"
-                     className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 max-w-screen-xl px-4 mx-0">
-                {teams?.map((team) => (
-                    <TeamCard
-                        key={team.teamId}
-                        teamId={team.teamId}
-                        teamName={team.teamName}
-                        projectName={team.projectName}
-                        liked={team.liked}/>
-                ))}
-
-            </section>
-
-        </div>
-    )
+        <section
+          aria-labelledby="allCards"
+          className="mx-0 grid max-w-screen-xl grid-cols-1 gap-4 px-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+        >
+          {teams?.map((team) => (
+            <TeamCard
+              key={team.teamId}
+              teamId={team.teamId}
+              teamName={team.teamName}
+              projectName={team.projectName}
+              liked={team.liked}
+            />
+          ))}
+        </section>
+      </div>
+    );
 
 };
 
