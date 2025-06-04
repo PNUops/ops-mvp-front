6bd6c22e9ac3a20e5b00e4ca5e0c327c0991da61
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-06-04 22:47:28 +0900

    design: 새 로고 이미지 적용

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="h-header z-20 flex w-full items-center justify-between px-6">
       <Link to="/">
-        <img className="h-10 w-auto" src="/Logo.png" alt="부산대학교 SW성과관리시스템 로고" />
+        <img className="h-10 w-auto" src="/Logo.svg" alt="부산대학교 SW성과관리시스템 로고" />
       </Link>
       <button className="text-exsm border-lightGray rounded-full border px-4 py-2">로그아웃</button>
     </header>
