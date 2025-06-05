848440cd369f4982f2affaaf13d71002f8a9d9ae
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-06-06 02:14:52 +0900

    feat: Api 호출 함수 postEmailVerificationPasswordReset 정의

--- src/apis/signIn.ts
+++ src/apis/signIn.ts
@@ -1,7 +1,11 @@
-import { SignInRequestDto, SignInResponseDto } from 'types/DTO';
+import { EmailVerificationRequestDTO, SignInRequestDto, SignInResponseDto } from 'types/DTO';
 import apiClient from './apiClient';
 
-export const postSignIn = async (credentials: SignInRequestDto): Promise<SignInResponseDto> => {
-  const response = await apiClient.post('/sign-in', credentials);
+export const postSignIn = async (request: SignInRequestDto): Promise<SignInResponseDto> => {
+  const response = await apiClient.post('/sign-in', request);
+  return response.data;
+};
+export const postEmailVerificationPasswordReset = async (request: EmailVerificationRequestDTO) => {
+  const response = await apiClient.post('/sign-in/password-reset/email-auth', request);
   return response.data;
 };
