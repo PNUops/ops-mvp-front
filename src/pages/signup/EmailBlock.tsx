a8a7a45b22de14842bb86c424f65ca650085b588
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-06-06 02:10:52 +0900

    refactor: EmailBlock 재사용을 위한 컴포넌트 리팩토링

--- src/pages/signup/EmailBlock.tsx
+++ src/pages/signup/EmailBlock.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import EmailRow from './EmailRow';
 import EmailVerifyRow from './EmailVerifyRow';
-import { error } from 'console';
+import { EmailVerificationRequestDTO } from 'types/DTO';
 
 interface Props {
   email: string;
@@ -9,9 +9,10 @@ interface Props {
   isEmailVerified: boolean;
   setIsEmailVerified: (value: boolean) => void;
   error?: string;
+  mutationFn: (request: EmailVerificationRequestDTO) => Promise<any>;
 }
 
-const EmailBlock = ({ email, setEmail, isEmailVerified, setIsEmailVerified, error }: Props) => {
+const EmailBlock = ({ email, setEmail, isEmailVerified, setIsEmailVerified, error, mutationFn }: Props) => {
   const EMAIL_VERIFY_COOLDOWN_SEC = 300;
   const [isMailSent, setIsMailSent] = useState(false);
   const [cooldown, setCooldown] = useState(0);
@@ -39,6 +40,7 @@ const EmailBlock = ({ email, setEmail, isEmailVerified, setIsEmailVerified, erro
         setIsMailSent={setIsMailSent}
         startCooldown={startCooldown}
         error={error}
+        mutationFn={mutationFn}
       />
       <EmailVerifyRow
         email={email}
