a4f3ede6eecb8fcb3a56336291ac36a54ce06bd9
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-06-06 02:24:49 +0900

    feat: Api 호출 함수 patchEmailVerificationCodePasswordReset 정의

--- src/apis/signIn.ts
+++ src/apis/signIn.ts
@@ -1,4 +1,9 @@
-import { EmailVerificationRequestDTO, SignInRequestDto, SignInResponseDto } from 'types/DTO';
+import {
+  EmailVerificationCodeRequestDTO,
+  EmailVerificationRequestDTO,
+  SignInRequestDto,
+  SignInResponseDto,
+} from 'types/DTO';
 import apiClient from './apiClient';
 
 export const postSignIn = async (request: SignInRequestDto): Promise<SignInResponseDto> => {
@@ -7,5 +12,9 @@ export const postSignIn = async (request: SignInRequestDto): Promise<SignInRespo
 };
 export const postEmailVerificationPasswordReset = async (request: EmailVerificationRequestDTO) => {
   const response = await apiClient.post('/sign-in/password-reset/email-auth', request);
+  return response.data;
+};
+export const patchEmailVerificationCodePasswordReset = async (request: EmailVerificationCodeRequestDTO) => {
+  const response = await apiClient.patch('/sign-in/password-reset/email-auth', request);
   return response.data;
 };
