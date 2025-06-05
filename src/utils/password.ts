d42691401aadb5e837fcaa716427d1a1d7ebc1ed
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-06-06 02:11:05 +0900

    feat: 비밀번호 검증 함수 로직 변경

--- src/utils/password.ts
+++ src/utils/password.ts
@@ -1,17 +1,18 @@
 /**
  * 비밀번호 형식의 유효성을 검사하는 함수.
  * 8~16자, 영어, 숫자, 특수문자 포함
+ * 특수문자는 @$!%*#?&~ 허용
  * @param password
  * @returns boolean
  */
 export const isValidPassword = (password: string) => {
-  const lengthRegex = /^.{8,16}$/;
+  const allowedCharRegex = /^[A-Za-z0-9@$!%*#?&~]{8,16}$/;
   const alphabetRegex = /[A-Za-z]/;
   const numberRegex = /[0-9]/;
-  const specialCharRegex = /[^A-Za-z0-9]/;
+  const specialCharRegex = /[@$!%*#?&~]/;
 
   return (
-    lengthRegex.test(password) &&
+    allowedCharRegex.test(password) &&
     alphabetRegex.test(password) &&
     numberRegex.test(password) &&
     specialCharRegex.test(password)
