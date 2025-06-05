7a9ad2f8c85b66caa3bc45f4be1d7bf705f98f6d
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-06-06 02:45:33 +0900

    design: TabSelector 패딩값 증가

--- src/pages/find/TabSelector.tsx
+++ src/pages/find/TabSelector.tsx
@@ -9,7 +9,7 @@ const TabSelector = ({ activeTab, setActiveTab }: Props) => {
   return (
     <div className="border-lightGray flex w-full overflow-hidden rounded-md border">
       <button
-        className={`flex-1 p-2 text-center font-semibold ${
+        className={`flex-1 p-3 text-center font-semibold ${
           activeTab === 'id' ? 'bg-mainBlue text-white' : 'text-midGray bg-whiteGray'
         }`}
         onClick={() => setActiveTab('id')}
@@ -17,7 +17,7 @@ const TabSelector = ({ activeTab, setActiveTab }: Props) => {
         아이디 찾기
       </button>
       <button
-        className={`flex-1 p-2 text-center font-semibold ${
+        className={`flex-1 p-3 text-center font-semibold ${
           activeTab === 'pw' ? 'bg-mainBlue text-white' : 'text-midGray bg-whiteGray'
         }`}
         onClick={() => setActiveTab('pw')}
