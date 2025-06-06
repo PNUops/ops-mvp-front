3dc9ed2c66c66496c53dac7df406993c22c14f01
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-06-06 12:21:19 +0900

    feat: 로그인 페이지에서 '/find'로 이동가능한 링크 추가

--- src/pages/signin/SignInPage.tsx
+++ src/pages/signin/SignInPage.tsx
@@ -1,12 +1,15 @@
+import { Link } from 'react-router-dom';
 import SignInForm from './SignInForm';
 
 const SignInPage = () => {
   return (
     <>
-      <div className="mx-auto flex max-w-2xl flex-col items-center p-10">
-        <img className="w-2/3" src="/Logo.svg" alt="부산대학교 SW성과관리시스템 로고" />
-        <h1 className="my-12 text-4xl font-bold">로그인</h1>
+      <div className="mx-auto flex max-w-2xl flex-col items-center gap-10 p-10">
+        <h1 className="my-8 text-4xl font-bold">로그인</h1>
         <SignInForm />
+        <Link className="text-midGray block" to="/find">
+          아이디/비밀번호 찾기
+        </Link>
       </div>
     </>
   );
