4b6490a1ab1b9601d43f7eea513635b4126482f0
HanseKim <kimbird2002@naver.com>
2025-06-06 11:10:09 +0900

    hotfix: layout 위아래 마진 추가

--- src/layout/FullContainer.tsx
+++ src/layout/FullContainer.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 const FullContainer = () => {
   return (
-    <div className="container mx-auto mb-20 px-4 sm:px-8 lg:px-16">
+    <div className="container mx-auto my-10 mb-64 px-4 sm:px-8 lg:px-16">
       <Outlet />
     </div>
   );
