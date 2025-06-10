3d386730168f67bcde39c226be5d20997ca1c8ce
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-06-11 06:03:44 +0900

    style: MainPage 불필요한 코드 제거

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,16 +1,14 @@
 import Notice from '@pages/main/Notice';
 import TotalCards from '@pages/main/TotalCards';
-import LeaderSection from "@pages/main/LeaderSection";
+import LeaderSection from '@pages/main/LeaderSection';
 
 const MainPage = () => {
   return (
-      <div className="relative flex flex-col gap-6 sm:gap-10">
-          <Notice/>
-          <div className="flex justify-end">
-              <LeaderSection/>
-          </div>
-          <TotalCards/>
-      </div>
+    <div className="flex flex-col gap-6 sm:gap-10">
+      <Notice />
+      <LeaderSection />
+      <TotalCards />
+    </div>
   );
 };
 
