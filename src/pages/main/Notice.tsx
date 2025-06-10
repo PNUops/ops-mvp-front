9495a63ca5f42b1a95b672c6e84d4f93d542af70
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-06-11 06:04:57 +0900

    design: 배너 최소 높이 지정

--- src/pages/main/Notice.tsx
+++ src/pages/main/Notice.tsx
@@ -1,13 +1,12 @@
 import banner from '@assets/banner.svg';
 
 const Notice = () => {
+  const BANNER_URL =
+    'https://swedu.pusan.ac.kr/swedu/31630/subview.do?enc=Zm5jdDF8QEB8JTJGYmJzJTJGc3dlZHUlMkY2OTA2JTJGMTcxNjIwOCUyRmFydGNsVmlldy5kbyUzRmJic09wZW5XcmRTZXElM0QlMjZpc1ZpZXdNaW5lJTNEZmFsc2UlMjZzcmNoQ29sdW1uJTNEc2olMjZwYWdlJTNEMSUyNnNyY2hXcmQlM0QlMjVFQyUyNUIwJTI1QkQlMjVFQyUyNTlEJTI1OTglMjVFQyUyNTlDJTI1QjUlMjVFRCUyNTk1JTI1QTklMjZyZ3NCZ25kZVN0ciUzRCUyNmJic0NsU2VxJTNEJTI2cGFzc3dvcmQlM0QlMjZyZ3NFbmRkZVN0ciUzRCUyNg%3D%3D';
   return (
-    <section aria-labelledby="notice" className="flex flex-col gap-4">
-        <a href="https://swedu.pusan.ac.kr/swedu/31630/subview.do?enc=Zm5jdDF8QEB8JTJGYmJzJTJGc3dlZHUlMkY2OTA2JTJGMTcxNjIwOCUyRmFydGNsVmlldy5kbyUzRmJic09wZW5XcmRTZXElM0QlMjZpc1ZpZXdNaW5lJTNEZmFsc2UlMjZzcmNoQ29sdW1uJTNEc2olMjZwYWdlJTNEMSUyNnNyY2hXcmQlM0QlMjVFQyUyNUIwJTI1QkQlMjVFQyUyNTlEJTI1OTglMjVFQyUyNTlDJTI1QjUlMjVFRCUyNTk1JTI1QTklMjZyZ3NCZ25kZVN0ciUzRCUyNmJic0NsU2VxJTNEJTI2cGFzc3dvcmQlM0QlMjZyZ3NFbmRkZVN0ciUzRCUyNg%3D%3D"
-           target="_blank">
-            <img src={banner} alt="대회 로고" className="cursor-pointer"/>
-        </a>
-    </section>
+    <a href={BANNER_URL} target="_blank" className="flex min-h-25">
+      <img src={banner} alt="대회 로고" className="flex cursor-pointer object-cover object-left" />
+    </a>
   );
 };
 
