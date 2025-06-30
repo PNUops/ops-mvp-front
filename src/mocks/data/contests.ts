d600da4ca07fcc4893ec0eddd03c27dea628f4c5
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-06-30 21:19:38 +0900

    feat: mockContestResponse 정의

---
+++ src/mocks/data/contests.ts
@@ -0,0 +1,14 @@
+import { ContestResponseDto } from 'types/DTO';
+
+export const mockContestsResponse: ContestResponseDto[] = [
+  {
+    contestId: 1,
+    contestName: '제6회PNU창의융합SW해커톤',
+    updatedAt: '2025-06-28T14:30:00Z',
+  },
+  {
+    contestId: 2,
+    contestName: '제5회PNU창의융합SW해커톤',
+    updatedAt: '2025-06-20T09:15:45Z',
+  },
+];
