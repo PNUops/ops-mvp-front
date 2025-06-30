e56ad27d4f059eab4334dde2cb1fa23bf39c8c3b
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-06-30 21:30:04 +0900

    fix: contestsHandler baseUrl 등록

--- src/mocks/handlers/contests.ts
+++ src/mocks/handlers/contests.ts
@@ -1,8 +1,10 @@
 import { mockContestsResponse } from '@mocks/data/contests';
 import { http, HttpResponse } from 'msw';
 
+const base = import.meta.env.VITE_API_BASE_URL ?? '';
+
 export const contestsHandler = [
-  http.get('/api/contests', () => {
+  http.get(`${base}/api/contests`, () => {
     return HttpResponse.json(mockContestsResponse);
   }),
 ];
