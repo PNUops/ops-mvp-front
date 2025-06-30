d49915c9f7abce34e964ce196cac87832b5d99ac
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-06-30 21:02:17 +0900

    feat: HeaderMenu 구현

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -4,6 +4,7 @@ import { BiCog } from 'react-icons/bi';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { useToast } from 'hooks/useToast';
+import { useState } from 'react';
 
 const Header = () => {
   const navigate = useNavigate();
@@ -18,20 +19,23 @@ const Header = () => {
 
   return (
     <header className="border-lightGray lg:h-header md:h-header xs:h-8 z-20 flex w-full min-w-[350px] items-center justify-between border-b bg-white px-4 py-2 sm:h-16">
-      <div className="mx-auto flex w-full items-center justify-between px-4 sm:px-8 lg:px-16">
+      <div className="mx-auto flex w-full items-center justify-between gap-4 px-4 sm:px-8 md:gap-8 lg:gap-16 lg:px-16">
         <Link to="/">
-          <img className="h-10 w-auto max-sm:hidden" src="/Logo.svg" alt="부산대학교 SW성과관리시스템 로고" />
+          <img className="w-auto max-sm:hidden md:h-9 lg:h-10" src="/Logo.svg" alt="부산대학교 SW성과관리시스템 로고" />
           <img
-            className="h-10 w-auto sm:hidden"
+            className="h-8 w-auto sm:hidden"
             src="/swOpsLogo-sm.png"
             alt="부산대학교 SW성과관리시스템 로고 (작은 버전)"
           />
         </Link>
-        <div className="flex items-center justify-between gap-8">
+
+        <HeaderMenu />
+
+        <div className="flex items-center justify-between gap-2 md:gap-4 lg:gap-8">
           {isAdmin && (
             <Link to="/admin" className="flex items-center gap-2 hover:cursor-pointer">
-              <BiCog className="text-mainGreen text-exsm cursor-pointer" />
-              <span className="text-exsm">관리자 페이지</span>
+              <BiCog className="text-mainGreen cursor-pointer text-sm" />
+              <span className="text-exsm hidden md:inline">관리자 페이지</span>
             </Link>
           )}
           {user?.name && (
@@ -52,3 +56,26 @@ const Header = () => {
 };
 
 export default Header;
+
+const HeaderMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="flex-1 font-semibold md:text-lg lg:text-xl">
+      <div className="relative inline-block" onMouseEnter={() => setIsOpen(true)} onMouseLeave={() => setIsOpen(false)}>
+        <span className="hover:text-mainGreen cursor-pointer">히스토리</span>
+        {isOpen && (
+          <div className="border-subGreen absolute top-full z-50 w-fit border-2 bg-white text-nowrap">
+            <ul className="text-base font-normal">
+              <li className="hover:text-mainGreen cursor-pointer px-4 py-2 hover:bg-gray-100">
+                제6회PNU창의융합SW해커톤
+              </li>
+              <li className="hover:text-mainGreen cursor-pointer px-4 py-2 hover:bg-gray-100">
+                제5회PNU창의융합SW해커톤
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
