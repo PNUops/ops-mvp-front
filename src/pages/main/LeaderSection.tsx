75e62ad4c20d9906677730d96a3d002e0b65c177
허동혁 <missko30@pusan.ac.kr>
2025-06-30 21:54:34 +0900

    fix: 개발용 주석 제거

--- src/pages/main/LeaderSection.tsx
+++ src/pages/main/LeaderSection.tsx
@@ -17,7 +17,7 @@ const LeaderSection = () => {
 
   const showLeaderMessage = isLeader && submissionData?.isSubmitted === false;
 
-  // if (!showLeaderMessage) return null;
+  if (!showLeaderMessage) return null;
   return (
     <div className="flex w-full items-center justify-between gap-2 rounded-lg bg-white p-2 text-sm shadow-md sm:gap-4 sm:p-4">
       <LeaderMessage leaderName={user?.name ?? '팀장'} />
