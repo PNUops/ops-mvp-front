bbffd47b444b0d002f4b738b320fe5132003c307
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-07-05 15:42:55 +0900

    feat: api 요청함수 getCurrentContestTeams, getContestTeams 정의

--- src/apis/contests.ts
+++ src/apis/contests.ts
@@ -1,7 +1,18 @@
 import { ContestResponseDto } from 'types/DTO';
 import apiClient from './apiClient';
+import { TeamListItemResponseDto } from 'types/DTO/teams/teamListDto';
 
 export const getAllContests = async (): Promise<ContestResponseDto[]> => {
   const res = await apiClient.get('/contests');
+  return res.data;
+};
+
+export const getCurrentContestTeams = async (): Promise<TeamListItemResponseDto[]> => {
+  const res = await apiClient.get('/contests/current/teams');
+  return res.data;
+};
+
+export const getContestTeams = async (contestId: number): Promise<TeamListItemResponseDto[]> => {
+  const res = await apiClient.get(`/contests/${contestId}/teams`);
   return res.data;
 };
