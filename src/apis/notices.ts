8bea1b2aed346c7d404c1b9aaae8164ee7fbe7d5
latteeea <kimtaeran4767@gmail.com>
2025-07-06 23:16:58 +0900

    feat: 공지사항 api 연결 및 빈 배열 시 텍스트 출력

--- src/apis/notices.ts
+++ src/apis/notices.ts
@@ -1,23 +1,14 @@
 
 import apiClient from './apiClient';
-import { mockNoticesResponse } from '@mocks/data/notices';
 import { NoticeDetailDto } from '../types/DTO/notices/NoticeDetailDto';
-import { mockNoticeDetail } from '@mocks/data/noticeDetail';
 
 export const getNotices = async () => {
-  // const { data } = await apiClient.get('/notices');
-  // return data;
-  return await mockNoticesResponse();
+  const { data } = await apiClient.get('/notices');
+  return data;
 };
 
 
 export const getNoticeDetail = async (noticeId: number): Promise<NoticeDetailDto> => {
-  // const { data } = await apiClient.get(`/notices/${noticeId}`);
-  // return data;
-  const notice = mockNoticeDetail[noticeId as keyof typeof mockNoticeDetail];
-
-  if (!notice) {
-    throw new Error ('404: 공지를 찾을 수 없습니다');
-  }
-  return notice;
+  const { data } = await apiClient.get(`/notices/${noticeId}`);
+  return data;
 };
--- src/pages/main/NoticeList.tsx
+++ src/pages/main/NoticeList.tsx
@@ -12,9 +12,14 @@ const NoticeList = () => {
   return(
     <div className="bg-white rounded-lg shadow p-4">
       <ul>
+        {notices && notices.length === 0 && (
+          <li className="text-midGray text-sm text-center py-2">
+            등록된 공지사항이 없습니다.
+          </li>
+        )}
         {notices?.map((notice) => (
           <li key={notice.noticeId}
-              className="flex items-center justify-between hover:bg-gray-100 rounded px-2 py-1 transition">
+              className="flex items-center justify-between hover:bg-lightGray rounded px-2 py-1 transition">
             <AiOutlineNotification className="mr-2" />
             <Link to={`/notices/${notice.noticeId}`} className="flex-1 truncate">
               {notice.title}
