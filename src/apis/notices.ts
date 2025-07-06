2afedc3c004b224275894e99d22efa08b0bfa76a
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-07-06 22:42:45 +0900

    feat: 공지사항 생성/수정 api 요청 함수 정의

--- src/apis/notices.ts
+++ src/apis/notices.ts
@@ -1,6 +1,7 @@
 import apiClient from './apiClient';
 import { NoticeResponseDto } from 'types/DTO/notices/NoticeResponseDto';
 import { NoticeDetailDto } from '../types/DTO/notices/NoticeDetailDto';
+import { NoticeRequestDto } from 'types/DTO/notices/NoticeRequestDto';
 
 export const getNotices = async (): Promise<NoticeResponseDto[]> => {
   const { data } = await apiClient.get('/notices');
@@ -9,5 +10,15 @@ export const getNotices = async (): Promise<NoticeResponseDto[]> => {
 
 export const getNoticeDetail = async (noticeId: number): Promise<NoticeDetailDto> => {
   const { data } = await apiClient.get(`/notices/${noticeId}`);
+  return data;
+};
+
+export const postCreateNotice = async (request: NoticeRequestDto) => {
+  const { data } = await apiClient.post('/notices', request);
+  return data;
+};
+
+export const patchNotice = async (noticeId: number, request: NoticeRequestDto) => {
+  const { data } = await apiClient.patch(`/notices/${noticeId}`, request);
   return data;
 };
