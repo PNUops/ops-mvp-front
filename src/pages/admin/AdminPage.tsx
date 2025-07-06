2521f501470be2cffb8728cca4d29f7856a0cbf0
HanseKim <kimbird2002@naver.com>
2025-07-06 16:15:49 +0900

    feat: tab 정보 url로 이동

--- src/pages/admin/AdminPage.tsx
+++ src/pages/admin/AdminPage.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useParams, useNavigate, useLocation } from 'react-router-dom';
 import OngoingContestsTab from '@pages/admin/OngoingContestsTab';
 import NoticeAdminPage from '@pages/admin/NoticeAdminTab';
 import ContestAdminPage from '@pages/admin/ContestAdminTab';
@@ -8,35 +9,51 @@ const adminTabs = [
   {
     label: '진행 중 대회',
     value: AdminTabType.ONGOING,
+    path: 'ongoing',
     component: <OngoingContestsTab />,
   },
   {
     label: '공지사항 관리',
     value: AdminTabType.NOTICE,
+    path: 'notice',
     component: <NoticeAdminPage />,
   },
   {
     label: '대회 관리',
     value: AdminTabType.CONTEST,
+    path: 'contest',
     component: <ContestAdminPage />,
   },
 ];
 
-function getInitialTab(): AdminTabType {
-  const currentTab = localStorage.getItem('adminTab');
-  if (currentTab !== null && !isNaN(Number(currentTab)) && Object.values(AdminTabType).includes(Number(currentTab))) {
-    return Number(currentTab) as AdminTabType;
-  }
-  return AdminTabType.ONGOING;
+function TabFromPath(path: string | undefined): AdminTabType {
+  if (!path) return AdminTabType.ONGOING;
+
+  const tab = adminTabs.find((tab) => tab.path === path);
+  return tab ? tab.value : AdminTabType.ONGOING;
 }
 
 const AdminPage = () => {
-  const [page, setPage] = useState<AdminTabType>(getInitialTab);
-  const currentTab = adminTabs.find((tab) => tab.value === page);
+  const { tab } = useParams();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [currentTab, setCurrentTab] = useState<AdminTabType>(TabFromPath(tab));
+
+  useEffect(() => {
+    if (location.pathname === '/admin') {
+      navigate('/admin/ongoing');
+      return;
+    }
+
+    const tabFromPath = TabFromPath(tab);
+    setCurrentTab(tabFromPath);
+  }, [tab, location.pathname, navigate]);
 
   function handleTabClick(tabValue: AdminTabType) {
-    setPage(tabValue);
-    localStorage.setItem('adminTab', tabValue.toString());
+    const tabItem = adminTabs.find((tab) => tab.value === tabValue);
+    if (tabItem) {
+      navigate(`/admin/${tabItem.path}`);
+    }
   }
 
   return (
@@ -47,14 +64,14 @@ const AdminPage = () => {
             key={tab.value}
             onClick={() => handleTabClick(tab.value)}
             className={`min-w-16 rounded-lg px-4 py-2 text-lg ${
-              page === tab.value ? 'bg-subGreen text-black' : 'bg-gray-100 text-gray-400'
+              currentTab === tab.value ? 'bg-subGreen text-black' : 'bg-gray-100 text-gray-400'
             }`}
           >
             {tab.label}
           </button>
         ))}
       </div>
-      {currentTab?.component}
+      {adminTabs.find((tab) => tab.value === currentTab)?.component}
     </div>
   );
 };
