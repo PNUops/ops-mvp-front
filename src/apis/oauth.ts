b957b8b4d8aa541471ee2a9433d32d6bdf490e93
redzzzi <myqueen1346@naver.com>
2025-07-07 01:51:56 +0900

    style: 파일명 변경

--- src/apis/oauth.ts
+++ src/apis/oauth.ts
@@ -1,6 +1,11 @@
 import apiClient from './apiClient';
 import { SignInResponseDto } from 'types/DTO';
 
+export const getGoogleOAuthLogin = async () => {
+  const response = await apiClient.get('/oauth/google');
+  return response.data;
+};
+
 export const getGoogleOAuthCallback = async (code: string): Promise<SignInResponseDto> => {
   const response = await apiClient.get('/oauth/google/callback', {
     params: { code },
