6150744e0115abb2684aea4d21a53d33ac2da2f4
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-07-06 22:11:47 +0900

    feat: 공지사항 GET api 요청 함수 정의

--- src/apis/notices.ts
+++ src/apis/notices.ts
@@ -1,23 +1,13 @@
-
 import apiClient from './apiClient';
-import { mockNoticesResponse } from '@mocks/data/notices';
+import { NoticeResponseDto } from 'types/DTO/notices/NoticeResponseDto';
 import { NoticeDetailDto } from '../types/DTO/notices/NoticeDetailDto';
-import { mockNoticeDetail } from '@mocks/data/noticeDetail';
 
-export const getNotices = async () => {
-  // const { data } = await apiClient.get('/notices');
-  // return data;
-  return await mockNoticesResponse();
+export const getNotices = async (): Promise<NoticeResponseDto[]> => {
+  const { data } = await apiClient.get('/notices');
+  return data;
 };
 
-
 export const getNoticeDetail = async (noticeId: number): Promise<NoticeDetailDto> => {
-  // const { data } = await apiClient.get(`/notices/${noticeId}`);
-  // return data;
-  const notice = mockNoticeDetail[noticeId as keyof typeof mockNoticeDetail];
-
-  if (!notice) {
-    throw new Error ('404: 공지를 찾을 수 없습니다');
-  }
-  return notice;
+  const { data } = await apiClient.get(`/notices/${noticeId}`);
+  return data;
 };
