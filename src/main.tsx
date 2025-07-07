601b6846d8865d32516f5540a7624973e0fd3013
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-07-07 09:27:45 +0900

    hotfix: Toast 나타나지 않는 문제 수정

--- src/main.tsx
+++ src/main.tsx
@@ -16,9 +16,7 @@ if (process.env.NODE_ENV === 'development') {
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={AppRoutes()}>
-        <App />
-      </RouterProvider>
+      <RouterProvider router={AppRoutes()} />
     </QueryClientProvider>
   </React.StrictMode>,
 );
