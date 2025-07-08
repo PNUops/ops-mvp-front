fec7283525be7f3f8e39f2d253ec7140d2a211f9
redzzzi <myqueen1346@naver.com>
2025-07-08 21:22:30 +0900

    feat: 이미지 폼데이터 생성 및 이미지 유효성 검사 유틸 함수 정의 - 단일 파일 또는 파일 배열인 경우에 따라 formData를 생성 - 이미지 파일에 대해 유효성 검사를 수행하고, 메시지를 반환   - 이미지 최대 크기를 5MB로 제한   - 이미지 파일 형식자를 jpeg, png, gif, bmp, webp로 제한

---
+++ src/utils/image.ts
@@ -0,0 +1,42 @@
+export const createImageFormData = (images: File | File[]) => {
+  const imageFormData = new FormData();
+  const imageArray = Array.isArray(images) ? images : [images];
+  imageArray.forEach((image) => imageFormData.append('image', image));
+  return imageFormData;
+};
+
+interface ImageValidateResult {
+  isValid: boolean;
+  message: string[];
+}
+
+export const imageValidator = (images: File | File[]): ImageValidateResult => {
+  const MAX_IMG_SIZE = 5 * (1024 * 1024); // 5MB
+  const ALLOWED_MIME_TYPES = ['image/jpeg', 'image/png', 'image/gif', 'image/bmp', 'image/webp'];
+
+  const imageValidateResult: ImageValidateResult = {
+    isValid: true,
+    message: [],
+  };
+
+  const imageArray = Array.isArray(images) ? images : [images];
+
+  for (const image of imageArray) {
+    if (!(image instanceof File)) {
+      imageValidateResult.isValid = false;
+      imageValidateResult.message.push('유효하지 않은 파일 객체예요');
+      return imageValidateResult;
+    }
+    if (image.size > MAX_IMG_SIZE) {
+      imageValidateResult.isValid = false;
+      imageValidateResult.message.push(`이미지 파일의 크기가 ${MAX_IMG_SIZE}를 초과해요`);
+      return imageValidateResult;
+    }
+    if (!ALLOWED_MIME_TYPES.includes(image.type)) {
+      imageValidateResult.isValid = false;
+      imageValidateResult.message.push(`${image.type}은 허용되지 않는 파일 형식이에요`);
+      return imageValidateResult;
+    }
+  }
+  return imageValidateResult;
+};
