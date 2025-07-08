06dbae278bcda2b3e0618cc7c961902a2dae0b92
redzzzi <myqueen1346@naver.com>
2025-07-08 22:12:04 +0900

    refactor: 이미지를 조회하는 커스텀 훅으로 분리

---
+++ src/hooks/useImages.ts
@@ -0,0 +1,43 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { getThumbnail } from 'apis/projectEditor';
+import { getPreviewImages } from 'apis/projectViewer';
+
+const useImages = (teamId: number, previewImageIds: number[]) => {
+  const {
+    data: thumbnailImage,
+    isLoading: isThumbnailImageLoading,
+    isError: isThumbnailImageError,
+  } = useQuery({
+    queryKey: ['thumbnailImage', teamId],
+    queryFn: async () => {
+      if (teamId === null) throw new Error('teamId is null');
+      return await getThumbnail(teamId);
+    },
+    enabled: teamId !== null,
+  });
+
+  const {
+    data: previewImages,
+    isLoading: isPreviewImagesLoading,
+    isError: isPreviewImagesError,
+  } = useQuery({
+    queryKey: ['previewImages', teamId, previewImageIds],
+    queryFn: async () => {
+      if (teamId === null || !previewImageIds) throw new Error('previewIds 없음');
+      return await getPreviewImages(teamId, previewImageIds);
+    },
+    enabled: teamId !== null && !!previewImageIds.length,
+  });
+
+  return {
+    thumbnailImage,
+    isThumbnailImageLoading,
+    isThumbnailImageError,
+    previewImages,
+    isPreviewImagesLoading,
+    isPreviewImagesError,
+  };
+};
+
+export default useImages;
