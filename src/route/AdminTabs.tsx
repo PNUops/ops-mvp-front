61b071fc2645cba0114ee16020dbb1d401ed52c7
redzzzi <myqueen1346@naver.com>
2025-07-08 13:36:48 +0900

    design: 대회 상태에 따라 수정 및 삭제가 불가능 하도록

--- src/route/AdminTabs.tsx
+++ src/route/AdminTabs.tsx
@@ -4,7 +4,6 @@ import OngoingContestsTab from '@pages/admin/OngoingContestsTab';
 import ManageNoticeListTab from '@pages/admin/NoticeManageTab/ManageNoticeListTab';
 import NoticeCreateTab from '@pages/admin/NoticeManageTab/NoticeCreateTab';
 import NoticeEditTab from '@pages/admin/NoticeManageTab/NoticeEditTab';
-import ProjectCreatorPage from '@pages/project-editor/ProjectCreatorPage';
 
 const AdminTabs: RouteObject[] = [
   { index: true, path: 'ongoing', element: <OngoingContestsTab />, handle: { label: '진행 중 대회' } },
@@ -23,10 +22,7 @@ const AdminTabs: RouteObject[] = [
   {
     path: 'contest',
     handle: { label: '대회 관리' },
-    children: [
-      { index: true, element: <ContestAdminTab /> },
-      { path: 'create/:contestId', element: <ProjectCreatorPage /> },
-    ],
+    children: [{ index: true, element: <ContestAdminTab /> }],
   },
 ];
 
