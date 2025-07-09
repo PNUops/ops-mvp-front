f572d90af5f3d244265f19af0696b6035dbed27f
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-07-09 22:55:37 +0900

    feat: MainLayout에 useAuthInit 적용

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -2,8 +2,12 @@ import Header from './Header';
 import Footer from './Footer';
 import FullContainer from './FullContainer';
 import { Toaster } from '@components/Toaster';
+import useAuthInit from 'hooks/useAuthInit';
 
 const MainLayout = () => {
+  const { isAuthInit } = useAuthInit();
+
+  if (!isAuthInit) return <></>;
   return (
     <>
       <div className="flex min-h-screen flex-col">
