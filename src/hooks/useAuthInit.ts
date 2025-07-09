2beddd69f42c7093df4dbbf57e946af1e89f82d5
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-07-09 22:55:15 +0900

    feat: 유저 상태 초기화 여부를 반환하는 useAuthInit 훅 구현

---
+++ src/hooks/useAuthInit.ts
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import { useTokenStore } from 'stores/useTokenStore';
+import { useUserStore } from 'stores/useUserStore';
+import { getUserFromToken } from 'utils/token';
+
+const useAuthInit = () => {
+  const { isTokenInit, initToken, token } = useTokenStore();
+  const { isUserInit: isAuthInit, setUser } = useUserStore();
+
+  useEffect(() => {
+    initToken();
+  }, [initToken]);
+
+  useEffect(() => {
+    if (!isTokenInit) return;
+    const decodedUser = token ? getUserFromToken(token) : null;
+    setUser(decodedUser);
+  }, [isTokenInit, token, setUser]);
+
+  return { isAuthInit };
+};
+export default useAuthInit;
