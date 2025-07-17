d719757f74e72599ad19df6dd4ef199d8f3fab14
redzzzi <myqueen1346@naver.com>
2025-07-17 14:46:58 +0900

    chore: 청크 분리

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,25 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), tailwindcss()],
+  plugins: [react(), tsconfigPaths(), tailwindcss(), visualizer({ open: true })],
   css: {
     postcss: './postcss.config.cjs',
   },
   build: {
     minify: 'esbuild',
     cssMinify: 'esbuild',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          recharts: ['recharts'],
+          vendor: ['axios', 'lodash', '@tanstack/react-query'],
+        },
+      },
+    },
   },
 });
