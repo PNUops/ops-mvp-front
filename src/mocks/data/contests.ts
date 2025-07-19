619a67b6575bab1e28d03d8dbd8541a2f1b37c14
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-07-20 01:06:59 +0900

    fix: mock 데이터 타입 수정

--- src/mocks/data/contests.ts
+++ src/mocks/data/contests.ts
@@ -4,21 +4,21 @@ export const mockContestsResponse: ContestResponseDto[] = [
   {
     contestId: 1,
     contestName: '제6회PNU창의융합SW해커톤',
-    updatedAt: '2025-06-28T14:30:00Z',
+    updatedAt: new Date('2025-06-28T14:30:00Z'),
   },
   {
     contestId: 2,
     contestName: '제5회PNU창의융합SW해커톤',
-    updatedAt: '2025-06-20T09:15:45Z',
+    updatedAt: new Date('2025-06-20T09:15:45Z'),
   },
   {
     contestId: 3,
     contestName: '제4회PNU창의융합SW해커톤',
-    updatedAt: '2025-06-20T09:15:45Z',
+    updatedAt: new Date('2025-06-20T09:15:45Z'),
   },
   {
     contestId: 4,
     contestName: '제3회PNU창의융합SW해커톤',
-    updatedAt: '2025-06-20T09:15:45Z',
+    updatedAt: new Date('2025-06-20T09:15:45Z'),
   },
 ];
