0b91c42260219ff3890c22cbb189b2f04207abb1
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-07-20 00:57:51 +0900

    fix: 필드 추가에 따른 api 호출 함수에서 Date 가공 로직 추가

--- src/apis/notices.ts
+++ src/apis/notices.ts
@@ -7,13 +7,13 @@ export const getNotices = async (): Promise<NoticeResponseDto[]> => {
   const { data } = await apiClient.get('/notices');
   return data.map((notice: NoticeResponseDto) => ({
     ...notice,
-    updatedAt: new Date(notice.updatedAt),
+    createdAt: new Date(notice.createdAt),
   }));
 };
 
 export const getNoticeDetail = async (noticeId: number): Promise<NoticeDetailDto> => {
   const { data } = await apiClient.get(`/notices/${noticeId}`);
-  return { ...data, updatedAt: new Date(data.updatedAt) };
+  return { ...data, updatedAt: new Date(data.updatedAt), createdAt: new Date(data.createdAt) };
 };
 
 export const postCreateNotice = async (request: NoticeRequestDto) => {
