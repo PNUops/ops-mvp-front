1d5d4352b4ca84f81160df2247e2c67508f4ef4c
Heo-DongHyuk <missko30@pusan.ac.kr>
2025-07-20 00:33:46 +0900

    fix: getNoticeDetail 호출부 수정

--- src/pages/notice/NoticeDetail.tsx
+++ src/pages/notice/NoticeDetail.tsx
@@ -1,9 +1,10 @@
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import { getNoticeDetail } from 'apis/notices';
-import { AiOutlineNotification } from "react-icons/ai";
+import { AiOutlineNotification } from 'react-icons/ai';
 import { useEffect } from 'react';
 import NoticeDetailSkeleton from './NoticeDetailSkeleton';
+import dayjs from 'dayjs';
 
 const NoticeDetail = () => {
   useEffect(() => {
@@ -11,7 +12,11 @@ const NoticeDetail = () => {
   }, []);
 
   const { noticeId } = useParams();
-  const { data: notice, isLoading, isError } = useQuery({
+  const {
+    data: notice,
+    isLoading,
+    isError,
+  } = useQuery({
     queryKey: ['noticeDetail', noticeId],
     queryFn: () => getNoticeDetail(Number(noticeId)),
     enabled: !!noticeId,
@@ -22,16 +27,17 @@ const NoticeDetail = () => {
     return <div>공지사항을 찾을 수 없습니다.</div>;
   }
 
-
   return (
-  <div className="max-w mx-auto">
-      <h1 className="text-2xl font-bold mb-6">공지사항</h1>
-      <div className="flex items-center bg-whiteGray rounded px-4 py-5 mb-2">
+    <div className="max-w mx-auto">
+      <h1 className="mb-6 text-2xl font-bold">공지사항</h1>
+      <div className="bg-whiteGray mb-2 flex items-center rounded px-4 py-5">
         <AiOutlineNotification className="mr-4" />
-        <span className="font-bold text-[clamp(0.85rem,2vw,1.3rem)] flex-1">{notice.title}</span>
+        <span className="flex-1 text-[clamp(0.85rem,2vw,1.3rem)] font-bold">{notice.title}</span>
       </div>
-      <div className="text-[clamp(0.7rem,1.5vw,0.9rem)] text-right text-midGray mb-6">{notice.updatedAt}</div>
-      <div className="text-[clamp(0.75rem,2vw,1rem)] bg-subGreen rounded p-6 leading-relaxed whitespace-pre-line">
+      <div className="text-midGray mb-6 text-right text-[clamp(0.7rem,1.5vw,0.9rem)]">
+        {dayjs(notice.updatedAt).format('YYYY년 M월 D일 HH:mm')}
+      </div>
+      <div className="bg-subGreen rounded p-6 text-[clamp(0.75rem,2vw,1rem)] leading-relaxed whitespace-pre-line">
         {notice.description}
       </div>
     </div>
